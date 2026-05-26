446d87a6211fdd57fb9a181d04bc5380fd191148
Rafael Lemes <rafael.lemes@gmail.com>
2026-05-26 08:38:40 -0300

    feat: add component without props

---
+++ src/components/Heading.tsx
@@ -0,0 +1,10 @@
+import styles from "./Heading.module.css";
+
+export function Heading() {
+  return (
+    <>
+      <h1 className={styles.heading}> Hello World </h1>
+      <h2> Pomodoro Timer</h2>
+    </>
+  );
+}
