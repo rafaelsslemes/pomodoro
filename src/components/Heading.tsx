2df37e1ddc2522f7f7f487668771eebd5421ddff
Rafael Lemes <rafael.lemes@gmail.com>
2026-05-28 08:26:05 -0300

    feat: different uses of properties

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,10 +1,17 @@
 import styles from "./Heading.module.css";
 
-export function Heading(props: { children: string; text: string }) {
+type HeadingProps = {
+  children: string;
+  text: string;
+};
+
+export function Heading(props: HeadingProps) {
+  const children = props.children; // destructuring
+
   return (
     <>
-      <h1 className={styles.heading}> {props.children} </h1>
-      <h2>{props.text}</h2>
+      <h1 className={styles.heading}> {children} </h1>
+      <h2>{props.text}</h2> // using props.text without destructuring
     </>
   );
 }
