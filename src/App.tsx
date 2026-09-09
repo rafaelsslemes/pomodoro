25bbba49751761da30dbc481e8ead1719d54c103
Rafael Lemes <rafael.lemes@gmail.com>
2026-09-09 08:08:51 -0300

    feat: add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Container } from "./components/Container";
-import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
       <Container>
-        <Heading>Menu</Heading>
+        <Menu />s
       </Container>
     </>
   );
